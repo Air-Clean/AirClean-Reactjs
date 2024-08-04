b2021f0e7857eb3d98e2756a8867fdd1a37edbdd
pyunghun <564564q@naver.com>
2024-08-04 20:31:51 +0900

    redux-persist 설치

--- src/Store.js
+++ src/Store.js
@@ -1,11 +1,22 @@
 import rootReducer from "./modules";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import ReduxThunk from 'redux-thunk'
+import ReduxThunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeWithDevTools(applyMiddleware(ReduxThunk))
 );
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
