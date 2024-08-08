5852d52ea4a7110bf237d696d5827d18c71177d7
comaserious <sciboviator@gmail.com>
2024-08-09 00:30:12 +0900

    [feature] laundry

--- src/common/LoadingSpinner.js
+++ src/common/LoadingSpinner.js
@@ -1,6 +1,18 @@
-import {MoonLoader} from 'react-spinners'
+
+import { useEffect, useState } from 'react';
 import laundryImage from '../assets/laundryvideo.gif'
-const Loading = () => {
+const Loading = ({delay = 1000}) => {
+
+  const [show, setShow] = useState(false);
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>setShow(true),delay);
+
+    return ()=> clearTimeout(timer);
+  },[delay])
+  if(!show){
+    return null
+  }
   return (
     <div className="loading" style={{display : 'flex' , alignItems : 'center', justifyContent : 'center' , zIndex : '4'}}>
       <img src={laundryImage} alt='이미지 없음'/>
